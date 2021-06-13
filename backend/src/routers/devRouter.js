9de5a741af00fd23b70dfda0e228356fd670993d
lirobinxc <lirobinxc@gmail.com>
2021-06-13 01:00:29 -0400

    backend: node added dev endpoint

--- backend/src/routers/devRouter.js
+++ backend/src/routers/devRouter.js
@@ -3,6 +3,7 @@ const devRouter = require('express').Router();
 devRouter.get('/node', (req, res) => {
   res.status(200).json({
     node_env: process.env.NODE_ENV,
+    database_url: process.env.DATABASE_URL,
   });
 });
 
